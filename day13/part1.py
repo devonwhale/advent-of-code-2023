cd173ffd3d7ace09d542e1ab8980ade17ec7e646
devonwhale <george@cattlebell.com>
2023-12-13 15:39:18 +0000

    Day 13 part 1

---
+++ day13/part1.py
@@ -0,0 +1,59 @@
+def calculate_score(mirror):
+    #print('Assessing mirror', mirror)
+    for index, row in enumerate(mirror):
+        adjusted_index = index + 1
+        if adjusted_index == len(mirror):
+            continue
+        left, right = mirror[:adjusted_index][::-1], mirror[adjusted_index:]
+        shortest_list = min(len(left), len(right))
+
+        valid = True
+        for i in range(0, shortest_list):
+            left_value = left[i]
+            right_value = right[i]
+            if left_value != right_value:
+                valid = False
+                break
+        
+        if valid:
+            #print('Mirror valid', left, right)
+            return adjusted_index
+        #else:
+            #print('Mirror not valid', left, right)
+    return -1
+
+def rotate(mirror):
+    rotated_mirror = {}
+    for char_index in range(len(mirror[0])):
+        if char_index not in rotated_mirror:
+            rotated_mirror[char_index] = ''
+        for line in mirror:
+            rotated_mirror[char_index] += line[char_index]
+    return [v for v in rotated_mirror.values()]
+
+
+def score(mirror):
+    s = calculate_score(mirror)
+    if s != -1:
+        return s * 100
+    
+    r = rotate(mirror)
+    return calculate_score(r)
+
+
+import os
+input = open(os.path.join(os.path.dirname(__file__), "input.txt"), 'r').read().splitlines()
+
+output = 0
+
+mirror_being_built = []
+for line in input:
+    if line == '':
+        output += score(mirror_being_built)
+        mirror_being_built = []
+    else:
+        mirror_being_built.append(line)
+output += score(mirror_being_built)
+
+
+print(output) #23955 too low
